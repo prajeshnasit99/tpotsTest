ef5f88e732d5f841afd3f5cd003da04fb33c4310
Prajesh <prajeshnasit99@gmail.com>
2026-02-25 13:03:47 +0530

    optimized code

--- app/code/TPots/JaPrajeshNasitFreeShippingBar/ViewModel/FreeShipping.php
+++ app/code/TPots/JaPrajeshNasitFreeShippingBar/ViewModel/FreeShipping.php
@@ -3,8 +3,6 @@
 namespace TPots\JaPrajeshNasitFreeShippingBar\ViewModel;
 
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Framework\Pricing\Helper\Data as PriceHelper;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
@@ -16,44 +14,18 @@ class FreeShipping implements ArgumentInterface
         'carriers/freeshipping/free_shipping_subtotal';
 
     /**
-     * @var checkoutSession
-     */
-    private CheckoutSession $checkoutSession;
-
-    /**
-     * @var PriceHelper
-     */
-    private PriceHelper $priceHelper;
-
-    /**
      * @var ScopeConfigInterface
      */
     private ScopeConfigInterface $scopeConfig;
 
     /**
      * Constructor
-     *
-     * @param CheckoutSession $checkoutSession
-     * @param PriceHelper $priceHelper
      * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        CheckoutSession $checkoutSession,
-        PriceHelper $priceHelper,
         ScopeConfigInterface $scopeConfig
     ) {
-        $this->checkoutSession = $checkoutSession;
-        $this->priceHelper = $priceHelper;
         $this->scopeConfig = $scopeConfig;
-    }
-
-    /**
-     * Get cart subtotal
-     */
-    public function getSubtotal(): float
-    {
-        $quote = $this->checkoutSession->getQuote();
-        return (float)$quote->getSubtotal();
     }
 
     /**
@@ -65,34 +37,5 @@ class FreeShipping implements ArgumentInterface
             self::XML_PATH_FREE_SHIPPING_MIN_AMOUNT,
             ScopeInterface::SCOPE_STORE
         );
-    }
-
-    /**
-     * Get Free Shipping Message based on cart subtotal and free shipping threshold
-     */
-    public function getFreeShippingMessage(): string
-    {
-        $subtotal  = $this->getSubtotal();
-        $threshold = $this->getFreeShippingThreshold();
-
-        if ($threshold <= 0) {
-            return '';
-        }
-
-        if ($subtotal <= 0) {
-            return __('Free Shipping on orders above %1',
-                $this->priceHelper->currency($threshold, true, false)
-            );
-        }
-
-        if ($subtotal < $threshold) {
-            $difference = $threshold - $subtotal;
-
-            return __('You’re %1 away from Free Shipping',
-                $this->priceHelper->currency($difference, true, false)
-            );
-        }
-
-        return __('Free shipping is applied to your order');
     }
 }
